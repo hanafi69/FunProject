7ce8662933b179de17bb720132552c5c70066b0b
Hanafi Hisyam <hanafi95@outlook.com>
2017-05-18 14:43:59 +0800

    Initial Commit

---
+++ IntoObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IntoObjectiveC
+//
+//  Created by Hanafi Hisyam on 18/05/2017.
+//  Copyright © 2017 Hanafi Hisyam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
